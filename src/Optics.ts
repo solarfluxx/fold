6071827f4f65c7849114285193ce9be70628f5fd
solarfluxx <56509699+solarfluxx@users.noreply.github.com>
2023-02-03 14:42:00 -0500

    Add cleanup for atoms focused inside of a react component

--- src/Optics.ts
+++ src/Optics.ts
@@ -1,9 +1,10 @@
+import { useEffect } from 'react';
 import { atomSymbol } from './Const';
 import { atom, createFeature } from './Main';
 import { Atom } from './Types';
 
 export function focusAtom<T extends { [key: string]: any }, U extends keyof T>(opticAtom: Atom<T>, property: U) {
-	return atom(
+	const sliceAtom = atom(
 		() => opticAtom.get()[property],
 		(incoming) => { opticAtom.do((value) => (value[property] = incoming)) }
 	).with(createFeature((_external, internal) => {
@@ -14,4 +15,13 @@ export function focusAtom<T extends { [key: string]: any }, U extends keyof T>(o
 		
 		return {};
 	}));
+	
+	try {
+		// If in a react component: cleanup the dependents on the origin when the component unmounts
+		useEffect(() => () => {
+			opticAtom[atomSymbol].dependents.delete(sliceAtom[atomSymbol]);
+		});
+	} catch {}
+	
+	return sliceAtom;
 }
